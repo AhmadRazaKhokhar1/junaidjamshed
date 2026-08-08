51841cd68fdbe47b2c6a8d31da18a2d37d79a454
AhmadRazaKhokhar1 <ahmadrazawebexpert@gmail.com>
2026-08-08 15:31:44 +0500

    set favicon

--- app/lib/ui/screens/about/index.tsx
+++
@@ -1,8 +0,0 @@
-import { WelcomeButtonAbout } from "../../screen-components/about/welcome-button"
-
-export const AboutScreen = () => {
-    return <div>
-        Hello this is an about screen component from screens/about/index.tsx
-        <WelcomeButtonAbout/>
-    </div>
-}
--- app/lib/ui/screens/signin/index.tsx
+++
@@ -1,5 +0,0 @@
-export const SignInScreen = () => {
-  return (
-    <div>Hello World</div>
-  )
-}
