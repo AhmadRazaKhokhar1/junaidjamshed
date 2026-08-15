e6188da2d18a0237411c2d5b8b58d1e005d2c71c
AhmadRazaKhokhar1 <ahmadrazawebexpert@gmail.com>
2026-08-15 16:15:59 +0500

    completed footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { NavBar } from "@lib/ui/useable-components";
+import { Footer, NavBar } from "@lib/ui/useable-components";
 
 
 const geistSans = Geist({
@@ -29,9 +29,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col justify-between">
         <NavBar/>
         {children}
+        <Footer/>
         </body>
     </html>
   );
