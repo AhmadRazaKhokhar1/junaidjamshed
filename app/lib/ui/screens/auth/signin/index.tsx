58808800fc7a46e0523af8d6d45bae948464ff04
AhmadRazaKhokhar1 <ahmadrazawebexpert@gmail.com>
2026-08-17 16:47:01 +0500

    update

--- app/lib/ui/screens/auth/signin/index.tsx
+++ app/lib/ui/screens/auth/signin/index.tsx
@@ -1,3 +1,7 @@
+import { LoginSection } from "@app/lib/ui/screen-components"
+
 export const SignInScreen = () => {
-    return <div className="flex size-full justify-center items-center">Hello SignIn</div>
+    return <div className="flex size-full justify-center items-center">
+        <LoginSection/>
+        </div>
 }
